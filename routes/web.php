ba079fc58be9ab8b4a85f57b81eb737a44689239
David <muhammaddavid81@gmail.com>
2025-02-19 16:30:18 +0700

    Tugas 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +21,51 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route :: get('/helo', function(){
+    return ' Hello World';
+});
+
+Route :: get('/world',function (){
+    return 'World';
+});
+
+Route :: get('/salam',function(){
+    return 'Selamat Datang';
+});
+
+Route :: get('/data', function(){
+    return '2341760076 <br> Muhammad Satria Rahmad David';
+});
+
+Route :: get('/user/{nama}',function($nama){
+    return 'Nama Saya : '.$nama;
+});
+
+Route::get('/posts/{a}/comments/{b}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route :: get('/article/{id}', function($id){
+    return "Halaman Artikel ke- : ".$id;
+});
+
+Route :: get('/user1/{name?}',function($name=null){
+    return "Nama : " .$name;
+});
+
+Route :: get('/user1/{name?}',function($name='john'){
+    return "Nama : " .$name;
+});
+
+Route :: get('/hello', [WelcomeController::class,'hello']);
+
+Route :: get('/home', [HomeController::class,'index1']);
+
+Route :: get('/about', AboutController::class);
+
+Route :: get('/article/{id}', ArticleController::class);
+
+Route :: resource('photos',PhotoController::Class);
+
+Route::get('/greeting', [WelcomeController::class,'greeting']);
